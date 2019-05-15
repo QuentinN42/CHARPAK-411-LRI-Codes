41fe580d05878e0b51432e61523455c70a948d6b
N42 <quentin.lieumont@gmail.com>
2019-05-15 16:13:57 +0200

    init -> choquet

--- Obj1/__init__.py
+++ Obj1/__init__.py
@@ -4,12 +4,4 @@ Creation d'un réseau de neurones pour regresser une integrale de choquet
 @date: 04/05/2019
 @author: Quentin Lieumont
 """
-from Obj1.test_network import run as simple_network
-from useful import generate, Network, Data, moy
-from keras.utils import plot_model
-
-
-def demo() -> Network:
-    net = simple_network(Data(generate(10, 10, dim=3), moy))
-    plot_model(net.model)
-    return net
+from useful.choquet import demo
