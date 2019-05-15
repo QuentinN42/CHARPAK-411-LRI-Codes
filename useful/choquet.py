6e0a5111a68dbd0fa8457cba707cbfbc54b870d5
N42 <quentin.lieumont@gmail.com>
2019-05-15 15:26:03 +0200

    main + dim added | history params removed

--- useful/choquet.py
+++ useful/choquet.py
@@ -7,7 +7,7 @@ Define a network that can regress a choquet function.
 """
 from useful.simpleNetwork import SimpleNetwork
 import numpy as np
-from useful.functions import nmap, two_by_two
+from useful.functions import nmap, two_by_two, generate, same_len
 from useful.data import Data
 
 
@@ -16,9 +16,13 @@ class Choquet:
                  w: np.array,
                  w_min: np.array,
                  w_max: np.array):
-        self.w = w
-        self.w_m = w_min
-        self.w_M = w_max
+        if not same_len([w, w_min, w_max]):
+            raise AttributeError('Vectors must have the same length')
+        else:
+            self.dim = len(w)
+            self.w = w
+            self.w_m = w_min
+            self.w_M = w_max
 
     def __call__(self, x: np.array) -> float:
         x_m = nmap(min, two_by_two(x))
@@ -27,7 +31,12 @@ class Choquet:
 
 
 class ChoquetData(Data):
-    def __init__(self, tab: np.array, func: Choquet = None, expected: np.array = None):
+    def __init__(self, tab: np.array = None, func: Choquet = None, expected: np.array = None):
+        if not tab:
+            if func:
+                tab = generate(dim=func.dim)
+            else:
+                raise AttributeError("Can't extract dimension")
         if func:
             super().__init__(tab, func)
         elif expected:
@@ -52,10 +61,15 @@ class ChoquetNetwork(SimpleNetwork):
 
                  # Training options
                  split_ratio: float = 0.5,
-                 validate: bool = True,
-                 plot_history: bool = False,
-                 plot_acc: bool = False,
-                 plot_loss: bool = False,
-                 save_link: str = ""
+                 validate: bool = True
                  ):
-        super().__init__(data, data.func, data.n_dim, use_bias, activation, split_ratio, validate, plot_history, plot_acc, plot_loss, save_link)
+        super().__init__(data, data.func, data.n_dim, use_bias, activation, split_ratio, validate)
+
+
+if __name__ == '__main__':
+    v1 = np.array([0.5, 5, 10])
+    v2 = np.array([1, 5, 2])
+    v3 = np.array([0.2, 0.1, 0.1])
+    ch = Choquet(v1, v2, v3)
+    chd = ChoquetData(func=ch)
+    net = ChoquetNetwork(chd)
