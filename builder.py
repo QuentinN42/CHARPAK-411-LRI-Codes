cfbcf91ca9169411c139ccea72af233e1c3ed224
N42 <quentin.lieumont@gmail.com>
2019-05-16 00:02:54 +0200

    2 dim data added

--- builder.py
+++ builder.py
@@ -2,10 +2,10 @@ from useful.data import Data
 from useful.choquet import Choquet
 import numpy as np
 
-v1 = np.array([0.5, 0.5, 0.8])
-v2 = np.array([0.1, 0.5, 0.2])
-v3 = np.array([0.2, 0.1, 0.5])
+v1 = np.array([0.5, 0.5])
+v2 = np.array([0.1])
+v3 = np.array([0.2])
 ch = Choquet(v1, v2, v3)
 chd = Data(func=ch, debug=True)
 
-chd.save('data/data.json')
+chd.save('learning_data/three.json')
