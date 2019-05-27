df7140dc55801a0fd01185d0c39ffd6a0d982b10
N42 <quentin.lieumont@gmail.com>
2019-05-27 17:30:29 +0200

    === Black ===

--- main.py
+++ main.py
@@ -2,8 +2,4 @@ from Obj2.real import build
 
 
 if __name__ == "__main__":
-    net = build(
-        save_folder="data/Obj2/real/remove1",
-        n_epochs=100,
-        n_build=25
-    )
+    net = build(save_folder="data/Obj2/real/remove1", n_epochs=100, n_build=25)
